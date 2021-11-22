b8e6ba0dc21c1c09b070e2bdab19da2cc29e8ec3
Arsenio Lourenco <43965372+ArsenioLourenco@users.noreply.github.com>
2021-11-22 17:04:37 +0100

    new file add

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-// const idTravel = document.querySelector('#idTravel')
-
-// const consultTravelBtn = document.querySelector('#consultTravel');
-
-// consultTravelBtn.addEventListener('click', (event) => {
-//     event.preventDefault();
-
-//     console.log('Consult Btn click event ');
-//     return consultTravel();
-// });
-
-
-// function consultTravel() {
-//     $.ajax({
-//         type: "GET",
-//         url: "http://192.168.4.97:6800/provinces/",
-//         dataType: "json",
-//         success: function (response) {
-//             console.log('Success: ', response);
-//         },
-//         error: function (response) {
-//             console.log('Error: ', response);
-//         }
-//     });
-// }
-
-$(document).ready(function () {
-    $('#').DataTable({
-        
-    });
-});
-=======
 // Selecionar a consulta com Id 
 const consultTravelBtn = $("#searchTravel"),
   searchResultTable = $("#searchResultTbl"),
@@ -239,5 +206,4 @@ verifySelectedTravel();
 
 
 
->>>>>>> 64a158fae8920a548445403e61954ddcf50885d0
 
